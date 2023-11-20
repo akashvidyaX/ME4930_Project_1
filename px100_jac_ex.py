dadc5e43dbd1232771be7cd661db959b84422653
mfdunlap <76010127+mfdunlap@users.noreply.github.com>
2023-11-20 09:38:17 -0600

    Update px100_jac_ex.py
    - Update quaternion to rotation matrix function

--- px100_jac_ex.py
+++ px100_jac_ex.py
@@ -200,9 +200,9 @@ class FrameListener(Node):
         q0, q1, q2, q3 = q0 / norm, q1 / norm, q2 / norm, q3 / norm
 
         # Calculate the rotation matrix elements
-        rotation_matrix = np.array([[1 - 2*(q2**2 + q3**2), 2*(q1*q2 - q3*q0), 2*(q1*q3 + q2*q0)],
-                                    [2*(q1*q2 + q3*q0), 1 - 2*(q1**2 + q3**2), 2*(q2*q3 - q1*q0)],
-                                    [2*(q1*q3 - q2*q0), 2*(q2*q3 + q1*q0), 1 - 2*(q1**2 + q2**2)]])
+        rotation_matrix = np.array([[q0**2+q1**2-q2**2-q3**2,       2*(q1*q2 - q3*q0),       2*(q1*q3 + q2*q0)],
+                                    [      2*(q0*q3 + q1*q2), q0**2-q1**2+q2**2-q3**2,       2*(q2*q3 - q1*q0)],
+                                    [      2*(q1*q3 - q0*q2),       2*(q2*q3 + q1*q0), q0**2-q1**2-q2**2+q3**2]])
 
         # Create a 4x4 homogeneous transformation matrix with zero translational elements
         homogeneous_matrix = np.eye(4)
